919a7c8b0984f064c4ae0dde3984bff67499b74c
QuerQue <ntomeky@gmail.com>
2017-03-11 18:27:07 +0100

    creating database, connecting with db

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,5 +1,53 @@
 package sample;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+import javafx.scene.control.Button;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 public class Controller {
+
+    Connection conn;
+
+    public void initialize()
+    {
+        try
+        {
+            Class.forName("java.sql.Driver");
+            conn = DriverManager.getConnection("jdbc:sqlite:D:/Programowanie/DatabaseManager/db/mydb.db");
+        } catch (Exception e)
+        {
+            System.out.println(e);
+        }
+    }
+
+    @FXML
+    TextArea textSpace;
+
+    @FXML
+    Button runButton;
+
+    @FXML
+    private void runButtonClicked() {
+        String query;
+        query = textSpace.getText();
+
+        Statement statement = null;
+
+        try
+        {
+            statement = conn.createStatement();
+            statement.setQueryTimeout(30);
+            statement.executeUpdate(query);
+        } catch (SQLException e)
+        {
+            e.printStackTrace();
+        }
+
+    }
 
 }
